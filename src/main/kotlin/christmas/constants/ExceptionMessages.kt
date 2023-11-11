fdac05be973350d44f7dcb1ba074377f1589b705
Kim Gimun <kmkim7575@gmail.com>
2023-11-11 23:31:23 +0900

    feat: InputViewMapperTest 문자열 형태로 입력받은 날짜를 검증하여 정수로 변환 기능 추가

--- src/main/kotlin/christmas/constants/ExceptionMessages.kt
+++ src/main/kotlin/christmas/constants/ExceptionMessages.kt
@@ -1,5 +1,6 @@
 package christmas.constants
 
 object ExceptionMessages {
-    const val EXCEPTION_DAY_INPUT_STRING = "문자열을 방문일로 입력할 수 없습니다. 1부터 31까지의 숫자를 입력해주세요."
+    const val EXCEPTION_DAY_INPUT_STRING = "문자열을 방문일로 입력할 수 없습니다. 1부터 31까지의 정수를 입력해주세요."
+    const val EXCEPTION_DAY_INPUT_RANGE = "예약일이 범위를 벗어났습니다. 1부터 31까지의 정수 중 하나를 입력해주세요."
 }
--- src/main/kotlin/christmas/utils/InputViewMapper.kt
+++ src/main/kotlin/christmas/utils/InputViewMapper.kt
@@ -1,9 +1,12 @@
 package christmas.utils
 
+import christmas.constants.ExceptionMessages.EXCEPTION_DAY_INPUT_RANGE
 import christmas.constants.ExceptionMessages.EXCEPTION_DAY_INPUT_STRING
 
-fun String.toDayNumber() = try {
-    this.toInt()
+fun String.toDayNumberOrThrowIllegalArgumentException() = try {
+    val dayNumber = this.toInt()
+    if (dayNumber !in 1..31) throw IllegalArgumentException(EXCEPTION_DAY_INPUT_RANGE)
+    dayNumber
 } catch (e: NumberFormatException) {
     throw IllegalArgumentException(EXCEPTION_DAY_INPUT_STRING)
 }
