a0ee190573edaa8e739deb0183a0bba096ab2757
Kim Gimun <kmkim7575@gmail.com>
2023-11-13 00:31:10 +0900

    feat: InputView의 주문 메뉴 입력 기능 추가

--- src/main/kotlin/christmas/view/InputView.kt
+++ src/main/kotlin/christmas/view/InputView.kt
@@ -1,15 +1,25 @@
 package christmas.view
 
 import camp.nextstep.edu.missionutils.Console
+import christmas.constants.Constants.SEPARATOR_ORDER_ITEM
 import christmas.constants.ExceptionMessages.ERROR
 import christmas.constants.ExceptionMessages.EXCEPTION_UNEXPECTED
 import christmas.constants.StepMessages.STEP_INPUT_DAY_OF_RESERVATION
+import christmas.constants.StepMessages.STEP_INPUT_ORDER
+import christmas.domain.OrderItem
+import christmas.utils.InputViewValidator
 import christmas.utils.toDayNumberOrThrowIllegalArgumentException
+import christmas.utils.toOrderItemOrThrowIllegalArgumentException
+import christmas.utils.toOrderListOrThrowIllegalArgumentException
 
 object InputView {
 
     fun getDayOfReservation(): Int {
         return getValidatedNumberOfDay()
+    }
+
+    fun getOrderList(): List<OrderItem> {
+        return getValidatedOrderList()
     }
 
     private fun getValidatedNumberOfDay(): Int = try {
@@ -18,7 +28,24 @@ object InputView {
         numberOfDay.toDayNumberOrThrowIllegalArgumentException()
     } catch (e: IllegalArgumentException) {
         println(getErrorMessage(e.message))
-        getDayOfReservation()
+        getValidatedNumberOfDay()
+    }
+
+    private fun getValidatedOrderList(): List<OrderItem> = try {
+        println(STEP_INPUT_ORDER)
+
+        val order = Console.readLine()
+            .toOrderListOrThrowIllegalArgumentException()
+        val orderItems = order.map {
+            it.toOrderItemOrThrowIllegalArgumentException()
+        }
+
+        InputViewValidator.validateOrders(orderItems)
+
+        orderItems
+    } catch (e: IllegalArgumentException) {
+        println(getErrorMessage(e.message))
+        getValidatedOrderList()
     }
 
     private fun getErrorMessage(message: String? = EXCEPTION_UNEXPECTED) = "$ERROR $message"
