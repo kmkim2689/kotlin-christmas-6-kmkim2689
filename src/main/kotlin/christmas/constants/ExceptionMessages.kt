0391d5b22d6c0a486d83030056caf2030df04f82
Kim Gimun <kmkim7575@gmail.com>
2023-11-13 00:16:28 +0900

    test: 메뉴 입력에 대한 유효성 검증 테스트 케이스 추가

--- src/main/kotlin/christmas/constants/ExceptionMessages.kt
+++ src/main/kotlin/christmas/constants/ExceptionMessages.kt
@@ -3,7 +3,8 @@ package christmas.constants
 object ExceptionMessages {
     const val ERROR = "[ERROR]"
 
-    const val EXCEPTION_DAY_INPUT = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."
+    const val EXCEPTION_DAY_INPUT = "$ERROR 유효하지 않은 날짜입니다. 다시 입력해 주세요."
+    const val EXCEPTION_ORDER_INPUT = "$ERROR 유효하지 않은 주문입니다. 다시 입력해 주세요."
 
     const val EXCEPTION_UNEXPECTED = "알 수 없는 에러가 발생하였습니다."
 }
--- src/test/kotlin/christmas/utils/InputViewValidatorTest.kt
+++ src/test/kotlin/christmas/utils/InputViewValidatorTest.kt
@@ -1,4 +1,64 @@
 package christmas.utils
 
+import christmas.domain.Menu
+import christmas.domain.OrderItem
+import christmas.view.InputView
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+
 class InputViewValidatorTest {
+
+    fun `주문 메뉴 정상적인 형태로 입력 시 정상 반환`() {
+        assertDoesNotThrow {
+            val input = listOf(
+                OrderItem(Menu.MUSHROOM_SOUP, 1),
+                OrderItem(Menu.CHOCOLATE_CAKE, 2),
+                OrderItem(Menu.TBONE_STEAK, 3)
+            )
+            InputViewValidator.validateOrders(input)
+        }
+    }
+
+    @Test
+    fun `주문 메뉴 입력 시 유효하지 않은 숫자를 입력한 경우 예외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            val input = listOf(
+                OrderItem(Menu.MUSHROOM_SOUP, 0),
+                OrderItem(Menu.CHOCOLATE_CAKE, 2),
+                OrderItem(Menu.TBONE_STEAK, 3)
+            )
+
+            InputViewValidator.validateOrders(input)
+        }
+    }
+
+    @Test
+    fun `주문 메뉴 입력 시 중복된 메뉴 입력 시 예외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            val input = listOf(
+                OrderItem(Menu.MUSHROOM_SOUP, 1),
+                OrderItem(Menu.MUSHROOM_SOUP, 2),
+                OrderItem(Menu.TBONE_STEAK, 3)
+            )
+
+            InputViewValidator.validateOrders(input)
+        }
+    }
+
+    @Test
+    fun `주문 메뉴 입력 시 총 개수가 20개를 넘어가는 경우 예외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            val input = listOf(
+                OrderItem(Menu.MUSHROOM_SOUP, 18),
+                OrderItem(Menu.MUSHROOM_SOUP, 2),
+                OrderItem(Menu.TBONE_STEAK, 3)
+            )
+
+            InputViewValidator.validateOrders(input)
+        }
+    }
+
+
 }
