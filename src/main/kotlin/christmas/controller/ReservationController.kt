91041b3a2dcf5600e3e2ed9d61aa6a9ff3c56716
Kim Gimun <kmkim7575@gmail.com>
2023-11-13 00:32:46 +0900

    feat: ReservationController에 메뉴 주문 로직 연결

--- src/main/kotlin/christmas/controller/ReservationController.kt
+++ src/main/kotlin/christmas/controller/ReservationController.kt
@@ -1,12 +1,13 @@
 package christmas.controller
 
 import christmas.constants.StepMessages.STEP_START_RESERVATION
-import christmas.domain.InputView
+import christmas.view.InputView
 
 object ReservationController {
     fun startReservation() {
         println(STEP_START_RESERVATION)
         val dayOfReservation = InputView.getDayOfReservation()
+        val orderItems = InputView.getOrderList()
     }
 
 }
