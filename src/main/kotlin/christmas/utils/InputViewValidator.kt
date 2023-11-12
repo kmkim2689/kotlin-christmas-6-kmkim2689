f0ab2e3cfb66d3c5d8c4858c4e433cb193f628df
Kim Gimun <kmkim7575@gmail.com>
2023-11-13 00:27:14 +0900

    feat: 입력받은 주문의 유효성을 검사하는 기능 추가

--- src/main/kotlin/christmas/utils/InputViewValidator.kt
+++ src/main/kotlin/christmas/utils/InputViewValidator.kt
@@ -1,4 +1,23 @@
 package christmas.utils
 
+import christmas.constants.ExceptionMessages.EXCEPTION_ORDER_INPUT
+import christmas.domain.OrderItem
+
 object InputViewValidator {
+
+    fun validateOrders(orders: List<OrderItem>) {
+        orders.onEach {
+            if (it.amount <= 0 || it.amount > 20) throwIllegalArgumentException()
+        }
+
+        val uniqueMenuCount = orders.map { it.menu }.toSet().size
+        if (uniqueMenuCount != orders.size) throwIllegalArgumentException()
+
+        val totalAmount = orders.sumOf { it.amount }
+        if (totalAmount > 20) throwIllegalArgumentException()
+    }
+
+    private fun throwIllegalArgumentException() {
+        throw IllegalArgumentException(EXCEPTION_ORDER_INPUT)
+    }
 }
