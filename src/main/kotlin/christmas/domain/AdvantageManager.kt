41ef14885b4a897b9111634a35752360e8f01c7b
Kim Gimun <kmkim7575@gmail.com>
2023-11-13 19:02:47 +0900

    feat: 디데이 할인 금액 반환 함수 추가

--- src/main/kotlin/christmas/domain/AdvantageManager.kt
+++ src/main/kotlin/christmas/domain/AdvantageManager.kt
@@ -1,18 +1,42 @@
 package christmas.domain
 
+import christmas.constants.Constants.ADVANTAGE_CRITERION_PRICE
+import christmas.constants.Constants.CATEGORY_DESSERT
+import christmas.constants.Constants.CATEGORY_MAIN
 import christmas.constants.Constants.CHAMPAGNE_PRESENTATION
 import christmas.constants.Constants.CHAMPAGNE_PRESENTATION_PRICE
+import christmas.constants.Constants.DISCOUNT_DDAY_DEFAULT
+import christmas.constants.Constants.DISCOUNT_DDAY_PER_DAY
 import christmas.constants.Constants.NONE
+import christmas.constants.Constants.PRICE_CHAMPAGNE
 
 class AdvantageManager(
     private val totalPrice: Int,
     private val reservationInfo: ReservationInfo
 ) {
 
+    private val dayOfReservation = reservationInfo.dayOfReservation
+
     fun getPresentationResult() = when (getFreeChampagneOrNot()) {
         true -> CHAMPAGNE_PRESENTATION
         false -> NONE
     }
 
+    fun getPresentationEventAmount() = when (getFreeChampagneOrNot()) {
+        true -> AdvantageItem("증정 이벤트", PRICE_CHAMPAGNE)
+        false -> AdvantageItem("증정 이벤트")
+    }
+
     fun getFreeChampagneOrNot() = totalPrice >= CHAMPAGNE_PRESENTATION_PRICE
+
+    fun getDdayDiscount(): AdvantageItem {
+        if (dayOfReservation in 1..25) {
+            return AdvantageItem(
+                "크리스마스 디데이 할인",
+                DISCOUNT_DDAY_PER_DAY * (dayOfReservation - 1) + DISCOUNT_DDAY_DEFAULT
+            )
+        }
+        return AdvantageItem("크리스마스 디데이 할인")
+    }
+
 }
