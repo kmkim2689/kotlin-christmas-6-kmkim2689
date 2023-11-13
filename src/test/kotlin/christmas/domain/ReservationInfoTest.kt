1af4253f74d9e32f80841b958ebede430d65b2ab
Kim Gimun <kmkim7575@gmail.com>
2023-11-13 20:07:21 +0900

    test: 할인 이전 총 주문 가격 반환에 대한 테스트 케이스 수정

--- src/test/kotlin/christmas/domain/ReservationInfoTest.kt
+++ src/test/kotlin/christmas/domain/ReservationInfoTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class ReservationInfoTest {
 
     @Test
-    fun `예약 정보를 통해 할인 이전 총 주문 가격을 문자열 형태로 출력`() {
+    fun `예약 정보를 통해 할인 이전 총 주문 가격을 정수로 출력`() {
         val dayOfReservation = 31
         val items = listOf(
             OrderItem(Menu.TBONE_STEAK, 2),
@@ -15,7 +15,7 @@ class ReservationInfoTest {
         val reservationInfo = ReservationInfo(dayOfReservation, items)
 
         val actualResult = reservationInfo.getTotalPriceBeforeDiscounts()
-        val expectedResult = "${55000 * 2 + 15000 * 2}"
+        val expectedResult = 55000 * 2 + 15000 * 2
 
         assertThat(actualResult).isEqualTo(expectedResult)
     }
